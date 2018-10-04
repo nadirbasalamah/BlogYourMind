008afe47c2cfd3f80bcb17f6473d965b8caa1fa7
Nadir <nadir008basalamah@gmail.com>
2018-10-04 08:30:10 +0700

    Adding bootstrap framework

--- ci/application/views/beranda.php
+++ ci/application/views/beranda.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <title>BlogYourMind</title>
   </head>
   <body>
