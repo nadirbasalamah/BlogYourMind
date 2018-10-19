c672fe159414d57eabc08c23681cea517a3d1452
Nadir <nadir008basalamah@gmail.com>
2018-10-19 09:16:40 +0700

    Improving UI in user's dashboard

--- ci/application/views/dasbor.php
+++ ci/application/views/dasbor.php
@@ -1,4 +1,5 @@
-<html>
+<!doctype html>
+<html lang="en">
 <?php
 if (isset($this->session->userdata['logged_in'])) {
 $nama = ($this->session->userdata['logged_in']['nama']);
@@ -7,25 +8,68 @@ $email = ($this->session->userdata['logged_in']['email']);
 header("location: login");
 }
 ?>
-<head>
-<title>Admin Page</title>
-</head>
-<body>
-<div id="profile">
-<?php
-echo "Hello <b id='welcome'><i>" . $nama . "</i> !</b>";
-echo "<br/>";
-echo "<br/>";
-echo "Welcome to Admin Page";
-echo "<br/>";
-echo "<br/>";
-echo "Your Username is " . $nama;
-echo "<br/>";
-echo "Your Email is " . $email;
-echo "<br/>";
-?>
-<b id="logout"><a href="<?php echo base_url('index.php/user_authentication/logout'); ?>">Logout</a></b>
-</div>
-<br/>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Halaman dasbor</title>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BlogYourMind</a>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="<?php echo base_url('index.php/user_authentication/logout'); ?>">Logout</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Dasbor <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file"></span>
+                  Daftar karya
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Sunting profil
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="layers"></span>
+                  Daftar favorit
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Selamat datang, <?php echo $nama; ?> ! </h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group mr-2">
+                <button class="btn btn-sm btn-outline-secondary">Tulis karya</button>
+                <button class="btn btn-sm btn-outline-secondary">Lihat karya</button>
+              </div>
+            </div>
+          </div>
+          <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
+        </main>
+      </div>
+    </div>
+  </body>
 </html>
