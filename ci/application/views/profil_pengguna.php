d8202596ff5ee9b67e7db388677fa62e33722e3f
Nadir <nadir008basalamah@gmail.com>
2018-11-14 17:30:43 +0700

    UI fixes for user's profile

--- ci/application/views/profil_pengguna.php
+++ ci/application/views/profil_pengguna.php
@@ -11,7 +11,6 @@ header("location: " . base_url('index.php/Pengguna/'));
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/starter-template.css" rel="stylesheet">
-  <link href="starter-template.css" rel="stylesheet">
     <title>BlogYourMind</title>
   </head>
   <body>
